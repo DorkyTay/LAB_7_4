7821866b4a3e4590e43c9392e665556eb5174aaf
DorkyTay <67008340+DorkyTay@users.noreply.github.com>
2020-07-31 20:28:05 -0400

    Add project files.

---
+++ DatingProfile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB_7_4
+{
+    class DatingProfile
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int age { get; set; }
+        public string gender { get; set; }
+        public string bio { get; set; }
+        public DatingProfile(string firstName, string lastName, int age, string gender)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.gender = gender;
+        }
+
+        public void WriteBio(string inputBio)
+        {
+            this.bio = inputBio;
+        }
+        public void ReadMessage()
+        {
+
+        }
+        public void SendMessage(string MessageTitle, string MessageData, DatingProfile toProfile)
+        {
+
+        }
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LAB_7_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from LAB_7_4");
+        }
+    }
+}
